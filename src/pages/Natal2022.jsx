8e5d335c82ca7f27c1c2b06fad6184422e4287d6
Rafaela <104620799+rafaeladev@users.noreply.github.com>
2023-05-09 17:31:20 +0200

    gallery test

--- src/pages/Natal2022.jsx
+++ src/pages/Natal2022.jsx
@@ -12,11 +12,11 @@ const Natal2022 = () => {
             <h1>Natal 2022</h1>
             <div className='container'>
                 {isLoading ? <div className='loader'></div> : <PhotoGallery data={data} />}
-                <img
-                    src='http://jlmsscf.cluster030.hosting.ovh.net/indianatijuca/Natal2022/PXL_20221222_180336256.jpg'
-                    alt='children'
-                />
             </div>
+            <img
+                src='http://jlmsscf.cluster030.hosting.ovh.net/indianatijuca/Natal2022/PXL_20221222_180336256.jpg'
+                alt='children'
+            />
         </>
     );
 };
